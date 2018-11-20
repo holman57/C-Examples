2870347f058ca60acabc7ed07c98573b8bfff76c
holman57 <holman57@gmail.com>
2018-11-20 12:07:01 -0600

    Added scanf
    scanf(..) is the normal way of parsing integers.

--- execlp.c
+++ execlp.c
@@ -4,16 +4,14 @@
 
 int main()
 {
-    char c, *ptr, *pString[] = {"pwd", "ls", "date", "who"};
+    char *pString[] = {"pwd", "ls", "date", "who"};
     int i = 0;
 
     printf(" 0 = pwd \n 1 = ls \n 2 = date \n 3 = who \nEnter Number: ");
-    c = (char) getchar();
-    i = (int) strtol(&c, &ptr, 10);
+    scanf("%d", &i);
 
     execlp(pString[i], pString[i], (char *) 0 );
     printf("Warning! execlp failed...\n");
 
     return 0;
 }
-
