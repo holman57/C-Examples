f06a92afc55eadcce5c378d2ddf04308151baaa1
holman57 <holman57@gmail.com>
2018-11-20 12:12:15 -0600

    Create proc1.c
    Example usage of forking a process into parent and child.

---
+++ proc1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    pid_t pid;
+    int i = 0, max_number = 150, minimum_number = 5;
+
+    srand((unsigned int) time(0));
+    int random = (rand() % (max_number - minimum_number + 1)) + minimum_number;
+
+    pid = fork();
+
+    printf("\t( pid = %d )\n", pid);
+
+    // Parent
+    if (pid > 0)
+    {
+        for (i = 0; i < random; i++)
+            printf("PARENT %d\n", i);
+    }
+    // Child
+    else
+    {
+        for (i = 0; i < random; i++)
+            printf("CHILD %d\n", i);
+    }
+
+    return 0;
+}
