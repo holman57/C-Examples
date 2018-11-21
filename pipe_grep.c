19ea82b23abaf1650ea6a03b1ad886edd9dfd1f9
holman57 <holman57@gmail.com>
2018-11-20 23:54:22 -0600

    Added more pipe examples.

---
+++ pipe_grep.c
@@ -0,0 +1,44 @@
+/*
+ * usage: ./a.out text_pattern input_file output_file
+ * Executes the command "grep text_pattern < input_file > output_file"
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main(int argc, char **argv)
+{
+    int ifp, ofp;
+
+    if (argc == 4)
+    {
+        // grep on text_pattern
+        char *grep_args[] = {"grep", argv[1], NULL};
+
+        // open input and output files
+        ifp = open(argv[2], O_RDONLY);
+        ofp = open(argv[3], O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
+
+        // duplicate input file to stdin
+        dup2(ifp, 0);
+
+        // duplicate output file to stdout
+        dup2(ofp, 1);
+
+        // close unused input file descriptor
+        close(ifp);
+
+        // close unused output file descriptor
+        close(ofp);
+
+        // execute grep
+        execvp("grep", grep_args);
+    }
+    else
+    {
+        printf("usage: %s text_pattern input_file output_file\n", argv[0]);
+    }
+}
---
+++ pipe_multi_branch.c
@@ -0,0 +1,110 @@
+/*
+ * usage: ./a.out input_file text_pattern
+ * Executes the command "cat input_file | grep text_pattern | cut -b 1-10".
+ * Note only minimal error checking is done for simplicity/shortness of code.
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <wait.h>
+
+int main(int argc, char **argv)
+{
+	int status;
+	int i;
+
+	if (argc == 3)
+	{
+		// arguments for commands
+		char *cat_args[]  = {"cat", argv[1], NULL};
+		char *grep_args[] = {"grep", argv[2], NULL};
+		char *cut_args[]  = {"cut", "-b", "1-10", NULL};
+
+		// file descriptors for 2 pipes: fd1 for cat-to-grep, fd2 for grep-to-cut
+		int fd1[2], fd2[2];
+
+        // make pipe for cat to grep
+		pipe(fd1);
+
+		// fd1[0] = read  end of cat->grep pipe (read by grep)
+		// fd1[1] = write end of cat->grep pipe (written by cat)
+
+        // make pipe for grep to cut
+		pipe(fd2);
+
+		// fd2[0] = read  end of grep->cut pipe (read by cut)
+		// fd2[1] = write end of grep->cut pipe (written by grep)
+
+		// fork the first child (to execute cat)
+		if (fork() == 0)
+		{
+			// duplicate write end of cat->grep pipe to stdout
+			dup2(fd1[1], 1);
+
+			// close both ends of all created fd# pipes (very important!)
+			close(fd1[0]);
+			close(fd1[1]);
+			close(fd2[0]);
+			close(fd2[1]);
+
+            execvp(*cat_args, cat_args);
+ 	 	}
+  		else // parent (assume no error)
+        {
+            // fork second child (to execute grep)
+            if (fork() == 0)
+			{
+	  			// duplicate read end of cat->grep pipe to stdin (of grep)
+				dup2(fd1[0], 0);
+
+	  			// duplicate write end of grep->cut pipe to stdout (of grep)
+				dup2(fd2[1], 1);
+
+	  			// close both ends of all created fd# pipes (very important!)
+				close(fd1[0]);
+				close(fd1[1]);
+				close(fd2[0]);
+				close(fd2[1]);
+
+	  			execvp(*grep_args, grep_args);
+			}
+                else // parent (assume no error)
+		{
+	  			// fork third child (to execute cut)
+	  			if (fork() == 0)
+                {
+                    // duplicate read end of grep->cut pipe to stadin (of cut)
+                    dup2(fd2[0], 0);
+
+                    // close both ends of all created fd# pipes (very important!)
+		    close(fd1[0]);
+		    close(fd1[1]);
+		    close(fd2[0]);
+	            close(fd2[1]);
+
+                    execvp(*cut_args, cut_args);
+                }
+			}
+        }
+      
+  		// only the parent gets here, close all pipes and wait for 3 children to finish
+  		close(fd1[0]);
+  		close(fd1[1]);
+  		close(fd2[0]);
+  		close(fd2[1]);
+
+  		for (i = 0; i < 3; i++)
+		{
+    		wait(&status);
+		}
+	}
+	else
+	{
+		printf("usage: %s input_file text_pattern\n", argv[0]);
+	}
+
+	return 0;
+}
