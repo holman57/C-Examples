9acbd80b89fb08677d5ed99d5ddd8a3b6b55b7c0
holman57 <holman57@gmail.com>
2020-07-24 22:13:35 -0500

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,26 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+bool isPalindrome(int x)
+{
+    int orig, temp;
+    long compare;
+    compare = 0;
+    orig = x;
+    if (x < 0)
+        return false;
+    while (orig != 0)
+    {
+        temp = orig % 10;
+        compare = compare * 10 + temp;
+        orig = orig / 10;
+    }
+    printf("compare: %ld\n", compare);
+    if (compare == x)
+    {
+        return true;
+    } else
+    {
+        return false;
+    }
+}
