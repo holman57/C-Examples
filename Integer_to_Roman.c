ae45f2808096dc3bbb5833a63f4b9a42340f716d
holman57 <holman57@gmail.com>
2020-07-27 13:26:45 -0500

    Update Integer_to_Roman.c

--- Integer_to_Roman.c
+++ Integer_to_Roman.c
@@ -10,27 +10,32 @@ Roman numerals are represented by seven different symbols: I, V, X, L, C, D and 
         D             500
         M             1000
  */
-char * intToRoman(int num) {
+char *intToRoman(int num)
+{
     // max is an arbitrary starting size for output which is a dynamicly sized char array with length len
     int max = 4, len = 0, shift = 0, cur;
-    char* output = (char*)malloc(max*sizeof(*output));
+    char *output = (char *) malloc(max * sizeof(*output));
     assert(output != NULL);
 
     char symbols[8] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
     int dist[10] = {1, 11, 111, 12, 2, 21, 211, 2111, 13, 3};
 
     // Look through the digits in num one at a time
-    while (num) {
-        if (num%10 != 0) {
+    while (num)
+    {
+        if (num % 10 != 0)
+        {
             // Find the corresponding roman numeral chars for the digit
-            cur = dist[num%10 - 1];
-            while (cur) {
+            cur = dist[num % 10 - 1];
+            while (cur)
+            {
                 // Double size of output array if necessary
-                if (len+1 >= max) {
-                    output = (char*)realloc(output, (max *= 2)*sizeof(*output));
-                    assert(output!=NULL);
+                if (len + 1 >= max)
+                {
+                    output = (char *) realloc(output, (max *= 2) * sizeof(*output));
+                    assert(output != NULL);
                 }
-                output[len++] = symbols[cur%10-1 + shift];
+                output[len++] = symbols[cur % 10 - 1 + shift];
                 cur /= 10;
             }
         }
@@ -41,10 +46,11 @@ char * intToRoman(int num) {
 
     // Reverse the output string
     int temp;
-    for (int i = 0; i < len/2; i++) {
+    for (int i = 0; i < len / 2; i++)
+    {
         temp = output[i];
-        output[i] = output[len-1-i];
-        output[len-1-i] = temp;
+        output[i] = output[len - 1 - i];
+        output[len - 1 - i] = temp;
     }
     return output;
 }
