60b750d2ad62be84f5ad197beec09f3ecc81750f
Luke Holman <holman57@gmail.com>
2024-12-30 12:54:29 -0600

    Add solution for Non-Divisible Subset problem
    Implemented the `nonDivisibleSubset` function to compute the maximum subset size with no two elements having a sum divisible by `k`. Includes input handling, logic for remainder counting, and main function setup for execution.

---
+++ JumpingOnTheClouds.c
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'jumpingOnClouds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY c as parameter.
+ */
+
+int jumpingOnClouds(vector<int> c) {
+    int jumps = 0; // To count the total number of jumps
+    int i = 0;     // Current position
+
+    // Traverse the cloud array
+    while (i < c.size() - 1) {
+        // If a jump of 2 is safe, make the jump
+        if (i + 2 < c.size() && c[i + 2] == 0) {
+            i += 2;
+        } else {
+            // Otherwise, jump only 1 cloud
+            i += 1;
+        }
+        jumps++; // Increment the jump counter
+    }
+
+    return jumps;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string c_temp_temp;
+    getline(cin, c_temp_temp);
+
+    vector<string> c_temp = split(rtrim(c_temp_temp));
+
+    vector<int> c(n);
+
+    for (int i = 0; i < n; i++) {
+        int c_item = stoi(c_temp[i]);
+
+        c[i] = c_item;
+    }
+
+    int result = jumpingOnClouds(c);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+string s(str);
+
+s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+);
+
+return s;
+}
+
+string rtrim(const string &str) {
+string s(str);
+
+s.erase(
+find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+);
+
+return s;
+}
+
+vector<string> split(const string &str) {
+vector<string> tokens;
+
+string::size_type start = 0;
+string::size_type end = 0;
+
+while ((end = str.find(" ", start)) != string::npos) {
+tokens.push_back(str.substr(start, end - start));
+
+start = end + 1;
+}
+
+tokens.push_back(str.substr(start));
+
+return tokens;
+}
