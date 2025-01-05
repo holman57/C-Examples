d6f2ef456c2d88ee337868feff622c2c52b85a95
Luke Holman <holman57@gmail.com>
2025-01-05 13:06:22 -0600

    Add `repeatedString` function to calculate occurrences of 'a'
    Implemented the `repeatedString` function to determine the number of 'a' characters in repeated substrings up to a given length. Included supporting functions for string handling and parsing input to ensure program robustness.

---
+++ RepeatedString.c
@@ -0,0 +1,166 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+
+long parse_long(char*);
+
+/*
+ * Complete the 'repeatedString' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. LONG_INTEGER n
+ */
+
+long repeatedString(char* s, long n) {
+    long string_length = strlen(s); // Length of the string s
+    long full_repeats = n / string_length; // Full repetitions of the string s
+    long remainder = n % string_length; // Remaining characters after full repetitions
+
+    // Count 'a' in the original string
+    long a_count = 0;
+    for (long i = 0; i < string_length; i++) {
+        if (s[i] == 'a') {
+            a_count++;
+        }
+    }
+
+    // Count 'a' in the remainder substring
+    long remainder_a_count = 0;
+    for (long i = 0; i < remainder; i++) {
+        if (s[i] == 'a') {
+            remainder_a_count++;
+        }
+    }
+
+    // Total count of 'a'
+    return (full_repeats * a_count) + remainder_a_count;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* s = readline();
+
+    long n = parse_long(ltrim(rtrim(readline())));
+
+    long result = repeatedString(s, n);
+
+    fprintf(fptr, "%ld\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+long parse_long(char* str) {
+    char* endptr;
+    long value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
